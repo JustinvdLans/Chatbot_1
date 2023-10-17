9092ffc176dff31a2f7901bb28ea9128b3466671
Justin van der Lans <justin.vanderlans@hva.nl>
2023-10-17 15:41:17 +0200

    added api key in a .env

--- my-app/pages/generate.js
+++ my-app/pages/generate.js
@@ -1,3 +1,15 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import OpenAI from "openai";
+import * as dotenv from 'dotenv';
+
+dotenv.config({path:__dirname + '/.env'});
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [],
+  temperature: 0.8,
+  max_tokens: 256,
+});
